1af35977e5d983f80051ea4303b2f46e2b3ee4cb
Swaroop P <soupspring47@gmail.com>
2019-12-24 20:33:07 +0530

    Update similarity.py
    Changed input of target hash to be a single value instead of multiple values as array. This was done to avoid further complication of acceptable data type.
    Now, The value_src and value_query is expected to be an integer or hexadecimal or array of binary or array of ints

--- imagewizard/similarity_metrics/api/similarity.py
+++ imagewizard/similarity_metrics/api/similarity.py
@@ -1,11 +1,27 @@
 from imagewizard.similarity_metrics.api.distance_algorithms import Similarity, hex_str_to_int
 
-def similarity(value_src: int, value_query: [int], metric: str ="hamming",) -> [int]:
-    """
-    The value_src is expected to be an integer or hex or binary or string of hex or binary
 
-    The value_query is expected to be either a simgle value or an array of integers or hex or binary 
-    or string of hex or binary.
+def format_value_to_binary_array(value) -> ([int]):
+    if isinstance(value, str):
+        # if value is hex string -> convert value to int
+        value = hex_str_to_int(value)
+    if isinstance(value, list):
+        # if value is list of string convert to list of int
+        value = list(map(int, value))
+    if isinstance(value, int):
+        # converting int to binary array
+        value = [int(x) for x in list('{0:0b}'.format(value))]
+    return (value)
+
+
+def similarity(
+        value_src: [int],
+        value_query: [int],
+        metric: str = "hamming",
+) -> int:
+    """
+    The value_src and value_query is expected to be an integer or hexadecimal or array of binary 
+    or array of ints
 
     The metric argument is the distance measurement metric. The values can be "hamming", "euclidean",
     "manhattan", "nroot", "cosine", "sqroot", "jaccard", "minkowski". The default is hamming.
@@ -15,32 +31,9 @@ def similarity(value_src: int, value_query: [int], metric: str ="hamming",) -> [
     
     """
     measures = Similarity()
+    value_src, value_query = format_value_to_binary_array(
+        value_src), format_value_to_binary_array(value_query)
 
-    # if value_src is hex string -> convert value_src to int
-    if isinstance(value_src, str):
-        value_src = hex_str_to_int(value_src)
-
-    # converting int to binary array
-    value_src = [int(x) for x in list('{0:0b}'.format(value_src))]
-
-    # if value_query is not list, convert value_query to list
-    if type(value_query) is not list: value_query = [value_query]
-
-    # if value_query is hex string -> convert value_query to int
-    for i, v in enumerate(value_query):
-        if isinstance(v, str):
-            value_query[i] = hex_str_to_int(v)
-
+    # hamming distance - similarity measurement
     if metric == "hamming":
-        # list to track hamming distance between 
-        # value_src to every element in value_query
-        dist = []
-
-        # get hamming distance
-        for val in value_query:
-            # converting int to binary array
-            val = [int(x) for x in list('{0:0b}'.format(val))]
-
-            dist.append(measures.manhattan_distance(value_src, val))
-        
-        return dist
+        return measures.manhattan_distance(value_src, value_query)
