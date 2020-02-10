4c4632514343d4e686966e76a217fb5eb1262d1f
Swaroop P <pswaroop1994@gmail.com>
2020-02-10 20:02:46 +0530

    made image_hashing\api as a module

--- setup.py
+++ setup.py
@@ -6,8 +6,13 @@ from setuptools import find_packages
 from setuptools import setup
 from pip.req import parse_requirements
 
+import pathlib
+
 REQ_GEN = parse_requirements('requirements.txt', session='hack')
 INSTALL_REQS = [str(ir.req) for ir in REQ_GEN]
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.rst").read_text()
 
 
 def read(filename):
@@ -18,22 +23,22 @@ def read(filename):
 
 
 setup(
-    name="imagewizard",
-    version="0.1.0",
-    url="https://github.com/Swaroop-p/imagewizard",
+    name="py-imagewizard",
+    version="0.1.1",
+    url="https://github.com/PriceSpider-NeuIntel/imagewizard",
     license='MIT',
 
     author="Swaroop Padala",
     author_email="soupspring47@gmail.com",
 
     description="imagewizard is a python based library for performing various image manipulations and operations",
-    long_description=read("README.rst"),
+    long_description=README,
 
     keywords = ['imagewizard', 'image hashing', 'hash', 'similarity', 'segmentation', 'image segmentation', 'image processing'],
 
     packages=find_packages(exclude=('tests','demo',)),
 
-    install_requires=INSTALL_REQS,
+    install_requires=['numpy==1.17.4', 'opencv-python==4.1.2.30', 'PyWavelets==1.1.1', 'pylint==2.4.4', 'scikit-learn==0.22.1', 'scipy==1.4.0', 'Pillow==6.2.1'],
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
