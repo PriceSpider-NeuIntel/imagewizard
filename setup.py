3307626e350ccd9cfdacf11cf0cda0e762af9daf
Swaroop P <pswaroop1994@gmail.com>
2020-02-10 20:09:55 +0530

    Added __init__.py to api folders to create them as modules

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(filename):
 
 setup(
     name="py-imagewizard",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/PriceSpider-NeuIntel/imagewizard",
     license='MIT',
 
